4d7760356853f0e14b5cc811ba56e476a6d648a2
Marek Pedrys <marek.pedrys@gmail.com>
2021-12-11 13:37:50 +0100

    ooo

---
+++ src/main/java/pl/marek/probka2/Probka2Application.java
@@ -0,0 +1,13 @@
+package pl.marek.probka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Probka2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Probka2Application.class, args);
+	}
+
+}
